1626573fde79e87a05b21eb7abaeccadc8383ca5
Andreas Lantz <48787829+AndreasLantz@users.noreply.github.com>
2022-03-24 18:25:12 +0100

    Create iterative.java

---
+++ Java/iterative.java
@@ -0,0 +1,22 @@
+public class binary{
+    
+    private static int searchBinary(int[] numArray, int target){
+        int middle, left = 0, right = numArray.length-1;
+        while (right >= left) {
+            middle = left + ((right - left) / 2);
+            if (numArray[middle] == target) return middle;
+            if (numArray[middle] > target) right = middle - 1;
+            if (numArray[middle] < target) left = middle + 1;
+        }
+    return -1;
+    }
+
+    public static void main(String args[])
+    {
+        int numArray[] = {1,3,4,7,8,12,14,18};
+        int target = 8;
+        int result = searchBinary(numArray, target);
+        
+        System.out.println(result == -1 ? -1 : result);
+    }
+}
