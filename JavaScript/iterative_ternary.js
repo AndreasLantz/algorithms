cc3ed79bcae080d42ba74767bfdb2ea1a6ff59a7
Andreas Lantz <48787829+AndreasLantz@users.noreply.github.com>
2022-03-24 21:17:09 +0100

    Spacing

--- JavaScript/iterative_ternary.js
+++ JavaScript/iterative_ternary.js
@@ -1,7 +1,7 @@
 // Iterative version that uses calculations in ternary operator expressions. Probably not as readable as separate if statements, but very compact.
 
 const binarySearch = (numArray, target) => {
-    let middle, left = 0, right = numArray.length-1;
+    let middle, left = 0, right = numArray.length - 1;
     while (right >= left) {
         middle = Math.floor(left + ((right - left) / 2));
         if (numArray[middle] === target) return middle;
