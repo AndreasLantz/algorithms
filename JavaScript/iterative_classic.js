c163eea512179fd6d24b9b4be142b1fa4a821925
Andreas Lantz <48787829+AndreasLantz@users.noreply.github.com>
2022-03-24 16:40:17 +0100

    Create iterative_classic.js
    A slightly older style just for fun.

---
+++ JavaScript/iterative_classic.js
@@ -0,0 +1,14 @@
+const binarySearch = (numArray, target) => {
+    let middle, left = 0, right = numArray.length-1;
+    while (right >= left) {
+        middle = Math.floor(left + ((right - left) / 2));
+        if (numArray[middle] == target) {
+            return middle;
+        } else if (numArray[middle] > target) {
+            right = middle - 1;
+        } else {
+            left = middle + 1;
+        }
+    }
+    return -1;
+};
