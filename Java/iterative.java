bfac15206c8f161d7e30e2d655f3e3f56767e6e0
Andreas Lantz <48787829+AndreasLantz@users.noreply.github.com>
2022-03-24 18:40:41 +0100

    Tidy-up

--- Java/iterative.java
+++ Java/iterative.java
@@ -1,6 +1,6 @@
 public class binary{
     
-    private static int searchBinary(int[] numArray, int target){
+    private static int searchBinary(int[] numArray, int target) {
         int middle, left = 0, right = numArray.length-1;
         while (right >= left) {
             middle = left + ((right - left) / 2);
@@ -11,8 +11,7 @@ public class binary{
     return -1;
     }
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         int numArray[] = {1,3,4,7,8,12,14,18};
         int target = 8;
         int result = searchBinary(numArray, target);
