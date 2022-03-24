2115092ef0dee1631716c53c2344f9c9924e9001
Andreas Lantz <48787829+AndreasLantz@users.noreply.github.com>
2022-03-24 21:16:38 +0100

    pivot -> middle

--- JavaScript/iterative_ternary.js
+++ JavaScript/iterative_ternary.js
@@ -1,11 +1,11 @@
 // Iterative version that uses calculations in ternary operator expressions. Probably not as readable as separate if statements, but very compact.
 
 const binarySearch = (numArray, target) => {
-    let pivot, left = 0, right = numArray.length-1;
+    let middle, left = 0, right = numArray.length-1;
     while (right >= left) {
-        pivot = Math.floor(left + ((right - left) / 2));
-        if (numArray[pivot] === target) return pivot;
-        (numArray[pivot] > target) ? right = pivot - 1 : left = pivot + 1;   
+        middle = Math.floor(left + ((right - left) / 2));
+        if (numArray[middle] === target) return middle;
+        (numArray[middle] > target) ? right = middle - 1 : left = middle + 1;   
     }
     return -1;
 };
